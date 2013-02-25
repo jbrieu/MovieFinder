aaf87940f5d5aa505d45937917fc52d83b12bc88
jbrieu <jbrieu@airfrance.fr>
2013-02-25 23:43:05 +0100

    empty applicatin project

---
+++ MovieFinder/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MovieFinder
+//
+//  Created by Jean-Baptiste Rieu on 25/02/13.
+//  Copyright (c) 2013 Jean-Baptiste Rieu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MFAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MFAppDelegate class]));
+    }
+}
---
+++ MovieFinderTests/MovieFinderTests.h
@@ -0,0 +1,13 @@
+//
+//  MovieFinderTests.h
+//  MovieFinderTests
+//
+//  Created by Jean-Baptiste Rieu on 25/02/13.
+//  Copyright (c) 2013 Jean-Baptiste Rieu. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MovieFinderTests : SenTestCase
+
+@end
