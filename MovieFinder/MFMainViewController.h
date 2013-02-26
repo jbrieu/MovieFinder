e2992058ac07bf1ff2bc63d5e399e29d7c9eb588
jbrieu <jbrieu@airfrance.fr>
2013-02-26 22:12:51 +0100

    Create Controller and Xib via template instead of manualy (like in last commi)

---
+++ MovieFinder/MFMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MFMainViewController.h
+//  MovieFinder
+//
+//  Created by Jean-Baptiste Rieu on 26/02/13.
+//  Copyright (c) 2013 Jean-Baptiste Rieu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MFMainViewController : UIViewController
+
+@end
---
+++ MovieFinder/MFMainViewController.m
@@ -0,0 +1,38 @@
+//
+//  MFMainViewController.m
+//  MovieFinder
+//
+//  Created by Jean-Baptiste Rieu on 26/02/13.
+//  Copyright (c) 2013 Jean-Baptiste Rieu. All rights reserved.
+//
+
+#import "MFMainViewController.h"
+
+@interface MFMainViewController ()
+
+@end
+
+@implementation MFMainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
